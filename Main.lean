759e27057a8311e7aa489ca69495b8fc8692af16
Sebastian Ullrich <sebasti@nullri.ch>
2023-03-01 13:40:27 +0100

    filter internal names

--- Main.lean
+++ Main.lean
@@ -5,8 +5,10 @@ def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot)
   let (imports, constants) := args.span (· != "--")
   let imports := imports.map fun mod => { module := Syntax.decodeNameLit ("`" ++ mod) |>.get! }
-  let constants? := constants.tail?.map (·.map fun c => Syntax.decodeNameLit ("`" ++ c) |>.get!)
   let env ← importModules imports {}
+  let constants := match constants.tail? with
+    | some cs => cs.map fun c => Syntax.decodeNameLit ("`" ++ c) |>.get!
+    | none    => env.constants.toList.map Prod.fst |>.filter (!·.isInternal)
   M.run env do
-    for c in constants?.getD (env.constants.toList.map (·.1)) do
+    for c in constants do
       let _ ← dumpConstant c
